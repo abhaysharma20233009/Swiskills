c98047cdd2e309f9a1ca2b1a7322895bd9be1f3d
Ravindra Rinwa <ravindrarinwa3@gmail.com>
2024-10-28 13:56:00 +0530

    Add global error handling middleware
    - Add sendErrorDev and sendErrorProd functions to handle detailed error messages in development and concise messages in production
    - Include handlers for common errors: database cast errors, duplicate fields, validation errors, JWT errors, and token expiration
    - Configure middleware to adapt based on environment (development vs. production) for secure and informative error responses

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const skillsRouter = require('./routes/skillsRouter');
+const skillsRouter = require('./routes/skillsRoutes');
+const userRouter = require('./routes/userRoutes');
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
 
 //Body parser,reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
 app.use('/api/v1/skills', skillsRouter);
+app.use('/api/v1/users', userRouter);
+
+app.use('*', (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+});
+
+//ERROR HEADLING MIDDLEWARE
+app.use(globalErrorHandler);
+
 module.exports = app;
---
+++ controllers/errorController.js
@@ -0,0 +1,72 @@
+const AppError = require('../utils/appError');
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational ,trusted error:send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    //Programming or other unknown error:don't leadk error details
+  } else {
+    //1)Log error
+    console.error('ERROR 💥', err);
+    //2)Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}:${err.value}.`;
+  return new AppError(message, 404);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.keyValue.name;
+  const message = `Duplicate field value:${value} Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invaild input data.${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const handleJWTError = () =>
+  new AppError('Invalid token.please log in again', 401);
+
+const handleTokenExpiredError = () =>
+  new AppError('Token is expried.please login again', 401);
+
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  if (process.env.NODE_ENV.trim() === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV.trim() === 'production') {
+    let error = JSON.parse(JSON.stringify(err));
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleTokenExpiredError();
+    sendErrorProd(error, res);
+  }
+};
