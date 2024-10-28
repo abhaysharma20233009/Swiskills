dffa919ab449dc59e939c356a525a05edda4d954
Ravindra Rinwa <ravindrarinwa3@gmail.com>
2024-10-28 13:24:08 +0530

    Implement user signup functionality with authentication and input validation

---
+++ controllers/authController.js
@@ -0,0 +1,56 @@
+const User = require('../models/userModel');
+const factory = require('./handlerFactory');
+const jwt = require('jsonwebtoken');
+const catchAsync = require('../utils/catchAsync');
+
+const signToken = (id) => {
+  //payload(data),jwt secret,jwt expire time
+  return jwt.sign(
+    {
+      id: id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    }
+  );
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  //Remove the password from the password
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  // const newUser = await User.create(req.body); //User.save(Wrong code because by this anyone make himself admin without any security flow)
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
+  });
+
+  createSendToken(newUser, 201, res);
+});
+
+exports.getAllUsers = factory.getAll(User);
