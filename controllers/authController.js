1529f343fbba4e1cf2adfd5c8cf31aa4a6192d2e
Ravindra Rinwa <ravindrarinwa3@gmail.com>
2024-10-28 14:32:01 +0530

    Add user login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/userModel');
 const factory = require('./handlerFactory');
 const jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 const signToken = (id) => {
   //payload(data),jwt secret,jwt expire time
@@ -51,6 +52,22 @@ exports.signup = catchAsync(async (req, res, next) => {
   });
 
   createSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  //1)Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password !', 400));
+  }
+  //2)Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+  //3)If everything ok,send token to client
+  createSendToken(user, 200, res);
 });
 
 exports.getAllUsers = factory.getAll(User);
