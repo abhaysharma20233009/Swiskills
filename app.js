b1e3e32254b7f1026d8f707e9514e920ce439c75
Abhay Sharma <abhaysharma21102006@gmail.com>
2024-11-05 16:52:52 +0530

    updated app.js

--- app.js
+++ app.js
@@ -8,25 +8,31 @@ const AppError = require('./utils/appError');
 const notificationRouter = require('./routes/notificationRoutes');
 const messagesRouter = require('./routes/messages');
 const errorHandler = require('./middleware/errorHandler');
+// Import your messages controller
 const dotenv = require('dotenv');
+
 dotenv.config();
-//Body parser,reading data from body into req.body
 
-
+// Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 
 app.use('/api/v1/skills', skillsRouter);
 app.use('/api/v1/users', userRouter);
-app.use('/api/v1/messages/', messagesRouter);
 app.use('/api/v1/notifications/', notificationRouter);
+app.use('/api/v1/messages/', messagesRouter);
 
 
 app.use('*', (req, res, next) => {
   next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
 });
 
-//ERROR HEADLING MIDDLEWARE
+// ERROR HANDLING MIDDLEWARE
 app.use(errorHandler);
 app.use(globalErrorHandler);
 
-module.exports = app;
+module.exports =  app; // Export both app and server instances
+
