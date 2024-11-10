2ebd4e9330397d84bac9b45577f289c18f47ad8f
Abhinav Yadav <abhinav.20233011@mnnit.ac.in>
2024-11-10 08:38:00 +0530

    edit in schema

--- models/ChatMessage.js
+++ models/ChatMessage.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const chatMessageSchema = new mongoose.Schema({
-  participants: {
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  },
+  //define a unique roomId fro two user's conversation 
+    roomId: { type:String, required: true },
   messages: [
     {
+      sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
       content: { type: String, required: true },
       status: {
         type: String,
-        enum: ['pending', 'sent'],
+        enum: ['pending', 'sent','edited','seen'],
         default: 'pending',
       },
       timestamp: { type: Date, default: Date.now },
